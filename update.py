2e1085eb76dc8b33ab7371f79c36788c0f9ad203
GitXBot101 <117470125+GitXBot101@users.noreply.github.com>
2023-10-23 20:31:56 +0530

    Update update.py

--- update.py
+++ update.py
@@ -37,7 +37,7 @@ try:
     if len(UPSTREAM_REPO) == 0:
        raise TypeError
 except:
-    UPSTREAM_REPO = "https://github.com/VigaBots-Repos/helios-heroku"
+    UPSTREAM_REPO = "https://github.com/arshsisodiya/helios-mirror"
 try:
     if len(UPSTREAM_BRANCH) == 0:
        raise TypeError
